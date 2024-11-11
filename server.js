ae08b0ae330525338fd51faa3e0a97e6b74ed1d2
Issah Akuffo <issahkuffo300@gmail.com>
2024-11-11 21:03:45 +0000

    update title

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-//Index route
-app.get("/", function(req, res){
-  res.render("index", {tittle: "Home"})
-})
-
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -27,6 +22,11 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
