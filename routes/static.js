76b263b4fbda483261826f1ee1445f6e4c6df611
Issah Akuffo <issahkuffo300@gmail.com>
2024-11-17 01:26:47 +0000

    update on index and partial

--- routes/static.js
+++ routes/static.js
@@ -8,6 +8,14 @@ router.use("/css", express.static(__dirname + "public/css"));
 router.use("/js", express.static(__dirname + "public/js"));
 router.use("/images", express.static(__dirname + "public/images"));
 
+router.use(express.static("views"));
+router.use("/partials", express.static(__dirname + "views/partials"));
+
+// Define the index route
+router.get('/', (req, res) => {
+    res.render('index'); // Render the EJS template located in the views folder
+});
+
 module.exports = router;
 
 
