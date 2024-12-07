08453573d4d1769de9f28123e67f4c788fc42445
Issah Akuffo <issahkuffo300@gmail.com>
2024-12-07 19:12:49 +0000

    module wk4

--- models/account-model.js
+++ models/account-model.js
@@ -1,15 +1,16 @@
 const pool = require("../database/")
 
 /* *****************************
-*   Register new account
-* *************************** */
-async function registerAccount(account_firstname, account_lastname, account_email, account_password){
-    try {
-      const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *"
-      return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
-    } catch (error) {
-      return error.message
-    }
+ *   Register new account
+ * *************************** */
+async function registerAccount(account_firstname, account_lastname, account_email, account_password) {
+  try {
+    const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *";
+    const result = await pool.query(sql, [account_firstname, account_lastname, account_email, account_password]);
+    return result.rows[0]; // Return the newly created account
+  } catch (error) {
+    return error.message;
   }
+}
 
-  module.exports = {registerAccount}
+module.exports = {registerAccount}
