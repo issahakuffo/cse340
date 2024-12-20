8bd0fa18bc8a94b6e13d9082fac5ea7e419d6101
issahakuffo <issahakuffo300@gmail.com>
2024-12-20 00:00:54 +0000

    database update

--- database/gw-wk2-sql.sql
+++ database/gw-wk2-sql.sql
@@ -6,7 +6,7 @@ CREATE TYPE public."account_type " AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public."account_type "
-    OWNER TO cse340ia;
+    OWNER TO cse340ak;
 
 
 CREATE TABLE public .classification (
